cd71c376e3b1c85d6868dc6de0dd53a6de0172e8
Twice_Ice <125768114+Twice-Ice@users.noreply.github.com>
2024-02-23 13:29:26 -0700

    misc small edits

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ object = [
 ]
 
 class Camera:
-    def __init__(self, pos, moveSpeed = 10):
+    def __init__(self, pos, moveSpeed = 10, turnSpeed = 2):
         self.pos = pos
         self.moveSpeed = moveSpeed
+        self.turnSpeed = turnSpeed
+        self.size = SCREEN_SIZE
+        self.normal = Vector2(0, 0)
     
     def update(self, keys):
         if keys[pygame.K_w]:
@@ -25,8 +28,16 @@ class Camera:
             self.pos.x += self.moveSpeed
         if keys[pygame.K_d]:
             self.pos.x -= self.moveSpeed
+        if keys[pygame.K_SPACE]:
+            self.pos.z += self.moveSpeed
+        if keys[pygame.K_LCTRL]:
+            self.pos.z -= self.moveSpeed
+        if keys[pygame.K_LEFT]:
+            self.normal.x -= self.turnSpeed
+        if keys[pygame.K_RIGHT]:
+            self.normal.x += self.turnSpeed
 
-camera = Camera(Vector2(0, 0))
+camera = Camera(Vector3(0, 0, 0))
 
 while not doExit:
     screen.fill((0, 0, 0))
@@ -37,15 +48,16 @@ while not doExit:
             doExit = True
     
     for i in range(len(object)):
-        p1 = object[i][0]# + camera.pos
-        p2 = object[i][1]# + camera.pos
-        p3 = object[i][2]# + camera.pos
-        print(p1)
         pointSize = 2
-        pygame.draw.polygon(screen, (155, 155, 155), (p1, p2, p3))
+        p1 = (object[i][0].x + camera.pos.x, object[i][0].z + camera.pos.z)
+        p2 = (object[i][1].x + camera.pos.x, object[i][1].z + camera.pos.z)
+        # p1 = object[i][0]# + camera.pos
+        # p2 = object[i][1]# + camera.pos
+        # p3 = object[i][2]# + camera.pos
+        # pygame.draw.polygon(screen, (155, 155, 155), (p1, p2, p3))
         pygame.draw.circle(screen, (255, 255, 255), p1, pointSize)
         pygame.draw.circle(screen, (255, 255, 255), p2, pointSize)
-        pygame.draw.circle(screen, (255, 255, 255), p3, pointSize)
+        # pygame.draw.circle(screen, (255, 255, 255), p3, pointSize)
 
     keys = pygame.key.get_pressed()
     camera.update(keys)
